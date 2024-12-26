d500af287500a3a391e70a4ef66f40b608638a8a
kunalspawar1827 <kunalspawar2191@gmail.com>
2024-12-26 17:39:05 +0530

    Adding Admin Class

--- HrAdminManagmentSystem/Program.cs
+++ HrAdminManagmentSystem/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HrAdminManagmentSystem;
 
 namespace HrAdminManagmentSystem
 {
@@ -10,9 +11,11 @@ namespace HrAdminManagmentSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("These Is Concole Application");
-            Console.WriteLine("New Commit");
-            Console.WriteLine("New Commit2");
+            //Console.WriteLine("These Is Concole Application");
+            //Console.WriteLine("New Commit");
+            //Console.WriteLine("New Commit2");
+            Admin admin = new Admin("Admin" , 1234);
+            
             Console.ReadLine();
 
         }
