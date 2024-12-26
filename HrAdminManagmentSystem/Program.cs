a62c59bac4e7d81584be004ec7f6ec072b1746f7
kunalspawar1827 <kunalspawar2191@gmail.com>
2024-12-26 15:44:51 +0530

    Add project files.

---
+++ HrAdminManagmentSystem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrAdminManagmentSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("These Is Concole Application");
+            Console.ReadLine();
+
+        }
+    }
+}
