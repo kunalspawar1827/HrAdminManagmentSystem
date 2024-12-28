f9433fdad06940a719fce0e044c5301a0784dd6c
kunalspawar1827 <kunalspawar2191@gmail.com>
2024-12-28 19:12:36 +0530

    Added Login Feacture To Code And Removed Admin Class Cauese It No Longer In Work - 28.12.2024 By Kunal

---
+++ HrAdminManagmentSystem/LoginDAL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrAdminManagmentSystem
+{
+    internal class LoginDAL
+    {
+
+        public bool IsValidLogin(string username, string password)
+        {
+            bool isValid = false;
+            try
+            {
+                if(( username == "Kunal" && password== "123"))
+                {
+                    isValid = true;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return isValid;
+        }
+        public   static void DisplyHeading()
+        {
+            try
+            {
+                Console.WriteLine("====== HR MANAGMENT SYSTEM ====== ");
+
+            }
+            catch (Exception ex) { }
+        }
+    }
+}
--- HrAdminManagmentSystem/Program.cs
+++ HrAdminManagmentSystem/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using HrAdminManagmentSystem;
 
@@ -11,11 +13,60 @@ namespace HrAdminManagmentSystem
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("These Is Concole Application");
-            //Console.WriteLine("New Commit");
-            //Console.WriteLine("New Commit2");
-            Admin admin = new Admin("Admin" , 1234);
-            
+
+            LoginDAL.DisplyHeading();
+            string Username, Pass;
+            LoginDAL loginDAL = new LoginDAL();
+
+            try
+            {
+                label:
+                Console.Write("Enter Username : ");
+                Username = Console.ReadLine().ToString();
+                if (!string.IsNullOrEmpty(Username) || Username != "")
+                {
+                    Console.Write("Enter Password : ");
+                    Pass = Console.ReadLine().ToString();
+                    bool res = loginDAL.IsValidLogin(Username, Pass);
+                    if (res)
+                    {
+                        /* Make Menu To Perform Operations */
+                        Console.Clear();
+                        LoginDAL.DisplyHeading();
+                        Console.WriteLine($"Welocome {Username}");
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Password Please Check Password Again Please Enter To Login Again");
+                        Console.ReadLine();
+                        Console.Clear();
+                        LoginDAL.DisplyHeading();
+
+                        goto label;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Usename Should Not Be Blank Please Enter To Login Again");
+                    Console.ReadLine();
+                    Console.Clear();
+                    LoginDAL.DisplyHeading();
+                    goto label;
+
+
+                }
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+
+            }
             Console.ReadLine();
 
         }
